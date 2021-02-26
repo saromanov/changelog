d4b3c3f5729106b9fe41b9387166a50f645efb95
Sergey <xxsmotur@gmail.com>
2021-02-27 00:16:03 +0500

    Move Write method to report.go

--- pkg/report/report.go
+++ pkg/report/report.go
@@ -1,6 +1,27 @@
 package report
 
-import "github.com/saromanov/changelog/pkg/models"
+import (
+	"fmt"
+	"os"
+
+	"github.com/saromanov/changelog/pkg/models"
+)
+
 type Report interface {
 	Do([]models.Message) error
+}
+
+// Write provides append changelog data to the file
+func Write(filename, data string) error {
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return fmt.Errorf("unable to open file %s: %v", filename, err)
+	}
+
+	defer f.Close()
+
+	if _, err = f.WriteString(data); err != nil {
+		return fmt.Errorf("unable to write to file: %v", err)
+	}
+	return nil
 }
--- pkg/report/txt/txt.go
+++ pkg/report/txt/txt.go
@@ -2,14 +2,13 @@ package txt
 
 import (
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/saromanov/changelog/pkg/models"
 	"github.com/saromanov/changelog/pkg/report"
 )
 
-// txt defines report for txt 
+// txt defines report for txt
 type txt struct {
 	filename string
 	title    string
@@ -34,20 +33,5 @@ func (t *txt) Do(data []models.Message) error {
 		result += fmt.Sprintf("%s %s (%s)\n", d.Date.Format(time.RFC3339), d.Message, d.Author)
 	}
 	result += "\n"
-	return write(t.filename, result)
-}
-
-// write provides append changelog data to the file
-func write(filename, data string) error {
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		return fmt.Errorf("unable to open file %s: %v", filename, err)
-	}
-
-	defer f.Close()
-
-	if _, err = f.WriteString(data); err != nil {
-		return fmt.Errorf("unable to write to file: %v", err)
-	}
-	return nil
+	return report.Write(t.filename, result)
 }
